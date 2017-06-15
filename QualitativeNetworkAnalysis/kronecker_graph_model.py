aa2a2f2ffb34ee29aef7d77b0f1295b41be7b08a
Carey Walker <carey_walker@dell.com>
2017-06-15 21:36:01 +0100

    Adding kronecker graph with outputs

--- QualitativeNetworkAnalysis/kronecker_graph_model.py
+++ QualitativeNetworkAnalysis/kronecker_graph_model.py
@@ -10,9 +10,71 @@ import utilities.common_functions as CommFunc
 import utilities.kronecker_product as Kron
 import utilities.average_accumulator as AvgAcc
 
-INITIATOR_GRAPH = [[0, 1], [2, 3]]
-POWER = 3
+VALUE1 = round(random.random(),1)
+VALUE2 = round(random.random(),1)
+VALUE3 = round(random.random(),1)
+VALUE4 = round(random.random(),1)
 
-KRONECKER_MATRIX = Kron.compute_kronecker_product(INITIATOR_GRAPH, POWER)
+INITIATOR_GRAPH = [[VALUE1, VALUE2], [VALUE3, VALUE4]]
+POWER = 3
+DECIMAL_PLACES = 3
+
+KRONECKER_MATRIX = Kron.compute_kronecker_product(INITIATOR_GRAPH, POWER, DECIMAL_PLACES)
 
 print("Length of matrix: ", len(KRONECKER_MATRIX))
+
+# for elem in INITIATOR_GRAPH:
+#     print(elem)
+
+# for elem in KRONECKER_MATRIX:
+#     print(elem)
+
+#convert KRONECKER_MATRIX into a numpy matrix which can then be used
+#as the data source for a graph
+A = np.matrix(KRONECKER_MATRIX)
+#create graph from numpy matrix
+G = nx.from_numpy_matrix(A)
+NUMBER_OF_NODES = nx.number_of_nodes(G)
+DEGREE_THRESHOLD = 1
+DEGREE_SEQUENCE = sorted(nx.degree(G, nbunch=None, weight=None).values(), reverse=True)
+A = sorted([degree for degree in np.asarray(DEGREE_SEQUENCE) if degree >= DEGREE_THRESHOLD], reverse=True)
+
+# and draw a histogram of the degree distribution
+pylab.hist(A, 50)
+pylab.grid(True)
+pylab.xlabel("Node Degree")
+pylab.ylabel("Number of Nodes")
+pylab.title("Kronecker Model Synthetic Network | Node Count: " + str(NUMBER_OF_NODES))
+pylab.show()
+
+# plot the log-log values
+DEGREE_LOG_VALUES = []
+RANK_LOG_VALUES = []
+for f in A:
+    RANK_LOG_VALUES.append(math.log(A.index(f)+1/NUMBER_OF_NODES))
+    DEGREE_LOG_VALUES.append(math.log(f))
+
+SLOPE, INTERCEPT, R_VALUE, P_VALUE, SLOPE_STD_ERROR = stats.linregress(DEGREE_LOG_VALUES, RANK_LOG_VALUES)
+
+print("Slope ", SLOPE)
+print("Y-intercept ", INTERCEPT)
+print("Alpha ", CommFunc.calculatealpha(SLOPE))
+
+X = np.array(DEGREE_LOG_VALUES)
+Y = np.array(RANK_LOG_VALUES)
+# Calculate some additional outputs
+PREDICT_Y = INTERCEPT + SLOPE * X
+PRED_ERROR = Y - PREDICT_Y
+DEGREES_OF_FREEDOM = len(X) - 2
+RESIDUAL_STD_ERROR = np.sqrt(np.sum(PRED_ERROR**2) / DEGREES_OF_FREEDOM)
+
+# Plotting
+pyl.plot(X, Y, 'o')
+pyl.plot(X, PREDICT_Y, 'k-')
+pyl.grid(True)
+plt.title("Kronecker Model Synthetic Network Degree Distribution | Node Count: " + str(NUMBER_OF_NODES))
+plt.ylabel("Log Rank")
+plt.xlabel("Log Degree")
+pyl.show()
+
+print("done")
--- QualitativeNetworkAnalysis/utilities/kronecker_product.py
+++ QualitativeNetworkAnalysis/utilities/kronecker_product.py
@@ -1,11 +1,12 @@
 
-def compute_kronecker_product(matrix, power):
+def compute_kronecker_product(matrix, power, decimalplaces):
     """
     Computes the kroneker product of the given nxn matrix
 
     Keyword arguments:
     matrix -- the nxn matrix
     power -- the number of times to multiply the matrix by itself
+    decimalplaces -- the number of decimal places to round values to
 
     NOTE: A 2x2 matrix with exponent = 3 generates a 256x256 matrix
     Using exponent > 3 is likely to cause an out of memory exception
@@ -26,7 +27,7 @@ def compute_kronecker_product(matrix, power):
             while check < count:
                 for num1 in elem1:
                     for num2 in kronecker_matrix[counter]:
-                        sub_matrix.append(num1 * num2)
+                        sub_matrix.append(round(num1 * num2, decimalplaces))
                 counter += 1
                 interim_matrix.append(sub_matrix)
                 sub_matrix = []
