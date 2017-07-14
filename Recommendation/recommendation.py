452c3925dec21ec040266ebafab815ce3c0d7753
Carey Walker <carey_walker@dell.com>
2017-07-14 19:21:21 +0100

    Fix for the 142 problem and adding cummulative rating

--- Recommendation/recommendation.py
+++ Recommendation/recommendation.py
@@ -20,32 +20,38 @@ G = nx.from_pandas_dataframe(trustdata, 'node1', 'node2')
 nx.info(G)
 
 #a simple check to see if there is an edge between two nodes
-def distance(user1, user2):
-    if G.has_edge(user1, user2):
-        return 2
+def distance(user1, user2, usetrust=False):
+    if usetrust:
+        if G.has_edge(user1, user2):
+            return 2
+        else:
+            user1Ratings = userProductRatingMatrix.transpose()[user1]
+            user2Ratings = userProductRatingMatrix.transpose()[user2]
+            return hamming(user1Ratings, user2Ratings)
     else:
         user1Ratings = userProductRatingMatrix.transpose()[user1]
         user2Ratings = userProductRatingMatrix.transpose()[user2]
         return hamming(user1Ratings, user2Ratings)
 
-def nearestNeighbours(user,K=10):
+def nearestNeighbours(user,K=10,usetrust=False):
     allUsers = pan.DataFrame(userProductRatingMatrix.index)
     allUsers = allUsers[allUsers.user!=user]
-    allUsers["distance"] = allUsers["user"].apply(lambda user2: distance(user, user2))
+    allUsers["distance"] = allUsers["user"].apply(lambda user2: distance(user, user2, usetrust))
     kNearestUsers = allUsers.sort_values(["distance"], ascending=True)["user"][:K]
     return kNearestUsers
 
-def getTopNProductsPerUser(user, K=10):
+def getTopNProductsPerUser(user, K=10, usetrust=False):
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=RuntimeWarning)
-        KNearestNeighbours = nearestNeighbours(user)
+        KNearestNeighbours = nearestNeighbours(user, K, usetrust)
         NearestNeighbourRatings = userProductRatingMatrix[userProductRatingMatrix.index.isin(KNearestNeighbours)]
         avgRating = NearestNeighbourRatings.apply(np.nanmean).dropna()
         productsAlreadyRated = userProductRatingMatrix.transpose()[user].dropna().index
         #remove average rating for products already rated by user
         avgRating = avgRating[-avgRating.index.isin(productsAlreadyRated)]
+        cummulative_rating = avgRating.sort_values(ascending=False)[:10].sum()
         topNCatProds = avgRating.sort_values(ascending=False).index[:K]
-    return topNCatProds
+    return topNCatProds, cummulative_rating
 
 RATING_MATRIX_FILE = "./data/rating.txt"
 ratings_df = pan.read_csv(RATING_MATRIX_FILE, sep=' ', names=['user', 'productids', 'category', 'rating', 'helpfulness', 'timestamp'])
@@ -60,9 +66,10 @@ productsPerUser = ratings_df.user.value_counts()
 ratings_df = ratings_df[ratings_df["productids"].isin(usersPerProduct[usersPerProduct>10].index)]
 userProductRatingMatrix = pan.pivot_table(ratings_df, values='rating', index=['user'], columns=['productids'])
 
-user = 1
+userid = 144
 topn = 10
+usetrust = True
 
-for line in userProductRatingMatrix:
-    products = getTopNProductsPerUser(line).values
-    print('User Id: ' + repr(line) + " || Recommended Products : " + ', '.join([str(product) for product in products]))
+for userid in userProductRatingMatrix.index:
+    products, cummulative_rating = getTopNProductsPerUser(userid, topn, usetrust)
+    print('User Id: ' + repr(userid) + " || Recommended Products : " + ', '.join([str(product) for product in products.values]) + " || Rating : " + str(cummulative_rating))
